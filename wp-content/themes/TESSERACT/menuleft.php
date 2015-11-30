86f932e7d93339b54640f27269e9bc9a8482958b
sucun21 <suk3c2uit@gmail.com>
2015-11-30 14:23:03 +0700

    get menu left follow categories <suk3c2uit@gmail.com>

--- wp-content/themes/TESSERACT/menuleft.php
+++ wp-content/themes/TESSERACT/menuleft.php
@@ -3,12 +3,20 @@
  *
  * @package Tesseract
  */
-
-if (is_home ()) { /* Does not appear on frontpage */
-	return;
-}
 ?>
 
 <div id="menu-left">
-    <?php wp_nav_menu( array( 'theme_location' => 'thirddary', 'menu_class' => 'nav-menu' ) );?>
+	<ul>
+	    <?php
+		    if (is_home ()) { /* Does not appear on frontpage */
+				return;
+			}
+			$categories = get_categories();
+			if ( ! empty( $categories ) ) {
+			    foreach( $categories as $category ) {
+			    	echo '<li><a class="menuleft" href="'.get_category_link( $category->term_id ).'">'.$category->name.'</li>';
+			    }
+			}
+		?>
+	</ul>
  </div>
