e9e10b927ef07e117c0fecb253eedecedaee6d38
sucun21 <suk3c2uit@gmail.com>
2015-12-01 14:40:35 +0700

    get parent-child categories <suk3c2uit@gmail.com>

--- wp-content/themes/TESSERACT/menuleft.php
+++ wp-content/themes/TESSERACT/menuleft.php
@@ -6,17 +6,12 @@
 ?>
 
 <div id="menu-left">
-	<ul>
 	    <?php
+	    	$args= array('title_li' => '' );
 		    if (is_home ()) { /* Does not appear on frontpage */
 				return;
 			}
-			$categories = get_categories();
-			if ( ! empty( $categories ) ) {
-			    foreach( $categories as $category ) {
-			    	echo '<li><a class="menuleft" href="'.get_category_link( $category->term_id ).'">'.$category->name.'</li>';
-			    }
-			}
+			$parents = wp_list_categories($args);
+			    	echo  $parent;
 		?>
-	</ul>
  </div>
