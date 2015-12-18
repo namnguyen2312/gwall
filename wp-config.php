4d4149386fc080444ae37e0cea9c925e6da0ee25
namnguyen2312 <namnd2312@gmail.com>
2015-12-18 14:42:08 +0700

    nam commit

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'greenwall');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'su123');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
